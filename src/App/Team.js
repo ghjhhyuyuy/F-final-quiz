6c2efed4f2ea366786f81d97dd148c8a29bcdecb
yourname <your@email.com>
2020-09-07 16:36:57 +0800

    Add Team.css

--- src/App/Team.js
+++ src/App/Team.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
 import Member from './Member';
+import './Team.css';
 
 class Team extends Component {
   render() {
     const members = (
-      <div>
+      <div className="members">
         {this.props.members.map((menber) => {
           return <Member name={menber.name} id={menber.id} key={menber.id} />;
         })}
       </div>
     );
     return (
-      <div>
-        <div>{this.props.name}</div>
+      <div className="team">
+        <div className="teamName">{this.props.name}</div>
         {members}
       </div>
     );
